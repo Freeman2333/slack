2c9d81f007d140c7215decb853fdd05cfbda92d9
Serhii Popov <serhii.popov@codeit.pro>
2025-10-23 17:05:05 +0300

    messages and reactions chemas fixed

--- convex/schema.ts
+++ convex/schema.ts
@@ -29,9 +29,27 @@ const schema = defineSchema({
     workspaceId: v.id("workspaces"),
     channelId: v.id("channels"),
     parentMessageId: v.optional(v.id("messages")),
-    // TODO: add conversationId
+    conversationId: v.optional(v.id("conversations")),
     updatedAt: v.number(),
-  }),
+  })
+    .index("by_workspace_id", ["workspaceId"])
+    .index("by_member_id", ["memberId"])
+    .index("by_channel_id", ["channelId"])
+    .index("by_conversation_id", ["conversationId"])
+    .index("by_channel_id_parent_message_id_conversation_id", [
+      "channelId",
+      "parentMessageId",
+      "conversationId",
+    ]),
+  reactions: defineTable({
+    workspaceId: v.id("workspaces"),
+    messageId: v.id("messages"),
+    memberId: v.id("members"),
+    value: v.string(),
+  })
+    .index("by_workspace_id", ["workspaceId"])
+    .index("by_message_id", ["messageId"])
+    .index("by_member_id", ["memberId"]),
 });
 
 export default schema;
